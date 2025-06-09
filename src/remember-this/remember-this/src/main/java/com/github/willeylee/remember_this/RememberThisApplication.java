42d63b4bc852e9095091af7aa5308f80a9700d7d
willey599 <willeyhlee@gmail.com>
2025-06-09 18:41:16 -0400

    feat(backend files): Created backend starter files via spring initializer

---
+++ src/remember-this/remember-this/src/main/java/com/github/willeylee/remember_this/RememberThisApplication.java
@@ -0,0 +1,13 @@
+package com.github.willeylee.remember_this;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberThisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RememberThisApplication.class, args);
+	}
+
+}
---
+++ src/remember-this/remember-this/src/test/java/com/github/willeylee/remember_this/RememberThisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.willeylee.remember_this;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememberThisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
