4aebf45ea1cef2053252b1cad7a94050a1fb0473
willey599 <willeyhlee@gmail.com>
2025-06-11 17:30:52 -0400

    feat(entities, pom.xml, application.properties): Create node and user entities and start work onbd connection

---
+++ src/remember-this-backend/remember-this/src/main/entities/Node.java
@@ -0,0 +1,16 @@
+package main.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "node")
+
+public class node {
+
+}
