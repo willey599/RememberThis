d303bf60ef1744b859b777674ac0320edb1ebedd
willey599 <willeyhlee@gmail.com>
2025-06-12 21:43:46 -0400

    some restructuring- not sure if this will break things

---
+++ RememberThis/src/app/app.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Cloud } from './cloud/cloud';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [Cloud],
+  template: `
+  <header class="main-header">Remember This</header>
+  <h1>
+    <app-cloud></app-cloud>
+  </h1>`,
+  styleUrl: './app.css',
+})
+
+export class App {
+  protected title = 'RememberThis';
+}
---
+++ RememberThis/src/app/cloud/cloud-create-menu/cloud-create-menu.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CloudCreateMenu } from './cloud-create-menu';
+
+describe('CloudCreateMenu', () => {
+  let component: CloudCreateMenu;
+  let fixture: ComponentFixture<CloudCreateMenu>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CloudCreateMenu]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CloudCreateMenu);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
