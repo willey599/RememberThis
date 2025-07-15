9809cfe17235d62fb0cae0091efe5a6ebe7f7b6d
willey599 <willeyhlee@gmail.com>
2025-07-15 18:11:36 -0400

    feat(CloudNodeRepository): Create cloud node repository

---
+++ remember-this-backend/src/main/java/com/willeylee/remember_this/repositories/CloudNodeRepository.java
@@ -0,0 +1,11 @@
+package com.willeylee.remember_this.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.willeylee.remember_this.entities.CloudNode;
+
+public interface CloudNodeRepository extends JpaRepository<CloudNode, Integer>{
+    Optional<CloudNode> findByUserId(int userId);
+}
