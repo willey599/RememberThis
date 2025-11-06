3aa3eff953bb787f6b1d0b1d529c3f0c220806a4
willey599 <willeyhlee@gmail.com>
2025-11-06 17:09:52 -0500

    change backend files to hardcode ingress URL for requests

--- remember-this-backend/src/main/java/com/willeylee/remember_this/configurations/SecurityConfig.java
+++ remember-this-backend/src/main/java/com/willeylee/remember_this/configurations/SecurityConfig.java
@@ -48,11 +48,11 @@ public class SecurityConfig {
             )
             .oauth2Login(oauth -> 
                 oauth.successHandler((request, response, authentication) -> {
-                    response.sendRedirect("http://localhost:4200/dashboard");
+                    response.sendRedirect("http://rememberthis.local/dashboard");
                 })
                 .userInfoEndpoint(userInfo -> userInfo.oidcUserService(customOidcUserService)))
             .logout(logout -> logout
-            .logoutSuccessUrl("http://localhost:4200")
+            .logoutSuccessUrl("http://rememberthis.local")
             );
 
         return http.build();
@@ -63,7 +63,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://rememberthis.local"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
